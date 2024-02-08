9ada69dfa2dc062f074a61ef5d7b97f952de87b8
rahul3603 <araderahul9196@gmail.com>
2024-02-08 19:16:03 +0530

    Give Animation to Navbar

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -13,40 +13,40 @@ export default function Navbar() {
         animate={{ y: 0 }}
         transition={{
           duration: 1,
-          ease:"easeInOut",
+          ease: "easeInOut",
         }}
         className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between py-3 navbar-expand-lg bg-white shadow"
       >
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-          <motion.div 
-          initial={{ x: -700 }}
-        animate={{ x: 0 }}
-        transition={{
-          duration: 1.5,
-          delay:0.6,
-          ease:"easeInOut",
-        }} 
-        >
-            <Link
-              to="/"
-              className="text-slate-700 text-lg font-bold leading-relaxed mr-4 whitespace-nowrap uppercase  flex items-center"
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <motion.div
+              initial={{ x: -700 }}
+              animate={{ x: 0 }}
+              transition={{
+                duration: 1.5,
+                delay: 0.7,
+                ease: "easeInOut",
+              }}
             >
-              <img
-                className="w-10 h-10 mr-2 inline-block"
-                src={require("../../assets/img/logo.png")}
-                alt=""
-              />
-              Rotaract Club
-            </Link>
-          </motion.div>
+              <Link
+                to="/"
+                className="text-slate-700 text-lg font-bold leading-relaxed mr-4 whitespace-nowrap uppercase  flex items-center"
+              >
+                <img
+                  className="w-10 h-10 mr-2 inline-block"
+                  src={require("../../assets/img/logo.png")}
+                  alt=""
+                />
+                Rotaract Club
+              </Link>
+            </motion.div>
             <motion.button
-          initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          duration: 1.5,
-          delay:1.5,
-        }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 1.5,
+                delay: 1.5,
+              }}
               className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
@@ -57,7 +57,7 @@ export default function Navbar() {
                 <i className="fa-solid fa-bars"></i>
               )}
             </motion.button>
-            </div>
+          </div>
           <div
             className={
               "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
@@ -65,7 +65,15 @@ export default function Navbar() {
             }
             id="example-navbar-warning"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <motion.ul
+              initial={{ y: -100 }}
+              animate={{ y: 0 }}
+              transition={{
+                duration: 1.5,
+                delay: 1.5,
+              }}
+              className="flex flex-col lg:flex-row list-none lg:ml-auto"
+            >
               <li className="flex items-center">
                 <Link>
                   <a
@@ -103,7 +111,7 @@ export default function Navbar() {
                 </a>
               </li>
 
-              <li className="flex items-center">
+              {/* <li className="flex items-center">
                 <Link to="/auth/login">
                   <button
                     className="bg-primary text-primary-content active:bg-primary-dark hover:bg-primary-dark text-sm font-semibold uppercase px-3 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
@@ -112,8 +120,8 @@ export default function Navbar() {
                     <i className="fa-solid fa-right-to-bracket"></i> Log In
                   </button>
                 </Link>
-              </li>
-            </ul>
+              </li> */}
+            </motion.ul>
           </div>
         </div>
       </motion.nav>
--- src/components/Sliders/HomeSlider.js
+++ src/components/Sliders/HomeSlider.js
@@ -14,84 +14,33 @@ export default function HomeSlider() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     arrows: false,
   };
   return (
     <>
       <div className="bg-sky-400 mx-auto mt-0 w-full h-full">
         <Slider {...settings}>
-          <div className="h-[100vh] bg-background">
-            <div className="flex flex-col h-full md:flex-row content-center items-center justify-evenly">
-              <div
-              
-                className="w-full md:w-3/12 flex justify-center"
-              >
-                <img
-                  className="w-36 h-36 md:w-44 md:h-44 mt-20 sm:mt-0"
-                  src={logo1}
-                  alt="Rotaract Club"
-                />
-              </div>
-              <div className="text-center w-full md:w-6/12">
-                <h1
-                 className="text-[36px] md:text-6xl font-bold p-2 md:p-4 font-[Poppins] tracking-wider text-black pb-3">
-                  ROTARACT CLUB
-                </h1>
-                <h1
-                className="text-xl md:text-3xl">Of</h1>
-                <h1 className="text-2xl md:text-4xl p-2 ">
-                  Rajarshi Shahu College of Engineering
-                </h1>
-                <h1 className="text-lg md:text-2xl text-gray-700">
-                  Sponsored by Rotary Club of Akurdi
-                </h1>
-                <h1
-                 className="text-lg md:text-2xl text-gray-700">
-                  Zone 03 | RID 3131
-                </h1>
-                <p 
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                transition={{
-                  duration:1.5,
-                  delay:3,
-                }}
-                className="mx-4 sm:mx-0 text-sm md:text-base text-gray-700 text-center w-5/15 mt-4">
-                  Rotaract clubs brings younger people together to exchange
-                  ideas with leaders in the community, develop leadership and
-                  professional skills, and have fun through service. In
-                  communities worldwide, Rotary and Rotaract members work side
-                  by side to take action through service.
-                </p>
-              </div>
-              <div
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                transition={{
-                  duration: 1.5,
-                }}
-                className="w-full md:w-3/12 flex justify-center"
-              >
-                <img
-                  className="w-36 h-36 md:w-44 md:h-44"
-                  src={logo2}
-                  alt="Rotaract Club"
-                />
-              </div>
-            </div>
-          </div>
-          {/* <div className=" h-[100vh] bg-white">
-            <div className="flex flex-col h-full md:flex-row content-center items-center justify-evenly">
+          <div className="h-[100vh] bg-white">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              whileInView={{ opacity: 1 }}
+              transition={{
+                duration: 1.5,
+                delay: 1.5,
+              }}
+              className="flex flex-col h-full lg:flex-row content-center items-center justify-evenly"
+            >
               <div className="w-full md:w-3/12 flex justify-center">
                 <img
-                  className="w-36 h-36 md:w-44 md:h-44"
+                  className="w-36 h-36 md:w-44 md:h-44 mt-20 lg:mt-0"
                   src={logo1}
                   alt="Rotaract Club"
                 />
               </div>
-              <div className="text-center w-full md:w-6/12">
-                <h1 className="text-[36px] md:text-6xl font-bold p-2 md:p-4 font-[Poppins] tracking-wider text-black">
+              <div className="text-center w-full lg:w-6/12">
+                <h1 className="text-[36px] sm:text-5xl md:text-6xl font-bold p-2 md:p-4 font-[Poppins] tracking-wider text-black pb-3">
                   ROTARACT CLUB
                 </h1>
                 <h1 className="text-xl md:text-3xl">Of</h1>
@@ -104,7 +53,47 @@ export default function HomeSlider() {
                 <h1 className="text-lg md:text-2xl text-gray-700">
                   Zone 03 | RID 3131
                 </h1>
-                <p className="text-sm md:text-base text-gray-700 text-center w-5/15 mt-2">
+                <p className="mx-4 md:mx-8 lg:mx-0 text-sm md:text-base text-gray-700 text-center w-5/15 mt-4">
+                  Rotaract clubs brings younger people together to exchange
+                  ideas with leaders in the community, develop leadership and
+                  professional skills, and have fun through service. In
+                  communities worldwide, Rotary and Rotaract members work side
+                  by side to take action through service.
+                </p>
+              </div>
+              <div className="w-full md:w-3/12 flex justify-center">
+                <img
+                  className="w-36 h-36 md:w-44 md:h-44"
+                  src={logo2}
+                  alt="Rotaract Club"
+                />
+              </div>
+            </motion.div>
+          </div>
+          {/* <div className="h-[100vh] bg-background">
+            <div className="flex flex-col h-full lg:flex-row content-center items-center justify-evenly">
+              <div className="w-full md:w-3/12 flex justify-center">
+                <img
+                  className="w-36 h-36 md:w-44 md:h-44 mt-20 lg:mt-0"
+                  src={logo1}
+                  alt="Rotaract Club"
+                />
+              </div>
+              <div className="text-center w-full lg:w-6/12">
+                <h1 className="text-[36px] sm:text-5xl md:text-6xl font-bold p-2 md:p-4 font-[Poppins] tracking-wider text-black pb-3">
+                  ROTARACT CLUB
+                </h1>
+                <h1 className="text-xl md:text-3xl">Of</h1>
+                <h1 className="text-2xl md:text-4xl p-2 ">
+                  Rajarshi Shahu College of Engineering
+                </h1>
+                <h1 className="text-lg md:text-2xl text-gray-700">
+                  Sponsored by Rotary Club of Akurdi
+                </h1>
+                <h1 className="text-lg md:text-2xl text-gray-700">
+                  Zone 03 | RID 3131
+                </h1>
+                <p className="mx-4 md:mx-8 lg:mx-0 text-sm md:text-base text-gray-700 text-center w-5/15 mt-4">
                   Rotaract clubs brings younger people together to exchange
                   ideas with leaders in the community, develop leadership and
                   professional skills, and have fun through service. In
